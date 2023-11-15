0a6dd105e41a384f1a6ac974d337b75ec5f57d10
Caleb <89772184+belacmu@users.noreply.github.com>
2023-11-15 12:09:27 +0100

    Small fixes

--- MyShop/React/src/index.js
+++ MyShop/React/src/index.js
@@ -1,10 +1,12 @@
 ﻿import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import Test from './Test'
 
 ReactDOM.render(
     <React.Fragment>
         <App />
+        <Test />
     </React.Fragment>,
     document.getElementById('root')
 );
